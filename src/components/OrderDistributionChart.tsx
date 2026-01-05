486fd18425a907c6ede91e26dc559218e79953ec
Superstar-IT <dima910516@gmail.com>
2026-01-05 14:52:49 -0500

    fix build error

--- src/components/OrderDistributionChart.tsx
+++ src/components/OrderDistributionChart.tsx
@@ -56,7 +56,7 @@ export default function OrderDistributionChart() {
             fill="#8884d8"
             dataKey="value"
           >
-            {data.map((entry, index) => (
+            {data.map((_entry, index) => (
               <Cell
                 key={`cell-${index}`}
                 fill={COLORS[index % COLORS.length]}
